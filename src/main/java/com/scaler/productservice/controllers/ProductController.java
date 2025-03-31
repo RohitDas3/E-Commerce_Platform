e985d058c08c3998750943118bfceb95c1761c58
Rohit Das <powerrangers.sami@gmail.com>
2025-03-31 14:32:07 +0530

    Added the initial packages

---
+++ src/main/java/com/scaler/productservice/controllers/ProductController.java
@@ -0,0 +1,16 @@
+package com.scaler.productservice.controllers;
+
+public class ProductController {
+
+    public void getAllProducts() {
+
+    }
+
+    public void getProductDetails(Long id) {
+
+    }
+
+    public void createProduct() {
+
+    }
+}
